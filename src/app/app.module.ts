bb65641f759b03596a6bc259815dcc6f12ade716
eureka <wangzhaosafe@163.com>
2019-12-14 14:27:18 +0800

    黑猫寿司！

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -8,6 +9,7 @@ import { GroupComponent } from './container/group/group.component';
 import { IndexComponent } from './container/index/index.component';
 import { LeftbarComponent } from './components/leftbar/leftbar.component';
 import { GroupiconComponent } from './components/groupicon/groupicon.component';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [
@@ -20,7 +22,9 @@ import { GroupiconComponent } from './components/groupicon/groupicon.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
